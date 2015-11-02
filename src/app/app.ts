b8701cb1ff6e876f6ca85f81f7d03c0e994464d4
como-quesito <rapidclimate@outlook.com>
2015-11-02 14:33:27 -0600

    Initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by campitos on 2/11/15.
+ */
+import {Component, bootstrap} from 'angular2/angular2';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App</h1>'
+})
+class AppComponent { }
+bootstrap(AppComponent);
+
