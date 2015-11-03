8d42ff05c2db7d6fbe7c550f362f5f4ca9e0173d
como-quesito <rapidclimate@outlook.com>
2015-11-02 20:15:30 -0600

    nov 2 2015 2:15

--- src/app/app3.ts
+++ src/app/app3.ts
@@ -2,19 +2,19 @@ import {bootstrap, Component, NgFor, View} from 'angular2/angular2';
 import {HTTP_BINDINGS, Http} from 'angular2/http';
 
 @Component({
-    selector: 'app2',
+    selector: 'app3',
     bindings: [HTTP_BINDINGS]
 })
 @View({
-    templateUrl: 'gente.html',
+    templateUrl: 'estacion.html',
     directives: [NgFor]
 })
 export class App {
-    people: Object[];
+    temperaturas: Object[];
     constructor(http:Http) {
-        http.get('http://127.0.0.1:49787/src/people.json')
+        http.get('http://campitos.elasticbeanstalk.com/estacion/temperatura')
             .map(res=>res.json())
-            .subscribe(people=>this.people=people);
+            .subscribe(temperaturas=>this.temperaturas=temperaturas);
     }
 
 }
